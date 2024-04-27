179f0f3920d19b2e0f56b8270280093ee1aec314
tarun <tarunrk2002@gmail.com>
2024-04-27 13:47:14 +0530

    added a model named cats and modified few things.

---
+++ webgreen/Models/cats.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webgreen.Models
+{
+    public class cats
+    {
+        [Key]
+        // we can also name it as catsid .. it will auto take that this is primary key
+        public int id { get; set; }
+        [Required]
+        // required means not null 
+        public string name { get; set; }
+        public int displayorder { get; set; }
+    }
+}
