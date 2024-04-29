63ed1963af91f2df279d5c3618233faafb347fdc
tarun <tarunrk2002@gmail.com>
2024-04-29 13:28:31 +0530

    configured database and added table category too .

--- webgreen/Program.cs
+++ webgreen/Program.cs
@@ -1,9 +1,14 @@
 
 
+using Microsoft.EntityFrameworkCore;
+using webgreen.data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -24,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=index}/{id?}");
 
 app.Run();
---
+++ webgreen/data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webgreen.Models;
+
+namespace webgreen.data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+
+        }
+
+        public DbSet<cats> categories { get; set; }
+    }
+}
