ccb963222db78e6c0ecc6fc114315f4fd2913386
tarun <tarunrk2002@gmail.com>
2024-04-29 19:29:54 +0530

    changes made to table and seeded some info to categories table

---
+++ webgreen/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webgreen.data;
+
+namespace webgreen.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        //public CategoryController(ApplicationDbContext db) 
+        //{
+        //   var g =  db.categories.ToList();
+        //}
+        public IActionResult Index()
+        {
+            return View("Category");
+        }
+
+        public IActionResult Next()
+        {
+            return View();
+        }
+
+    }
+}
--- webgreen/Migrations/20240429125932_pushingsomedataintothetable.Designer.cs
+++ webgreen/Migrations/20240429125932_pushingsomedataintothetable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webgreen.data;
 
@@ -10,9 +11,11 @@ using webgreen.data;
 namespace webgreen.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240429125932_pushingsomedataintothetable")]
+    partial class pushingsomedataintothetable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
