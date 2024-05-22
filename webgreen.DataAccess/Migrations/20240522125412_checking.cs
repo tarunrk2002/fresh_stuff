46f4a432b855cedb6a2e90692e1ef4aa73c3eb84
tarun <tarunrk2002@gmail.com>
2024-05-22 18:26:19 +0530

    ...package manager console.

---
+++ webgreen.DataAccess/Migrations/20240522125412_checking.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webgreen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class checking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    displayorder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
