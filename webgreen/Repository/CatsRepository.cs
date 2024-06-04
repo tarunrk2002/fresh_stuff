06bd1f28c6ce79d06f718384c7176931f13f6ff3
tarun <tarunrk2002@gmail.com>
2024-06-04 18:18:04 +0530

    category done

---
+++ webgreen/Repository/CatsRepository.cs
@@ -0,0 +1,25 @@
+﻿using webgreen.DataAccess.data;
+using webgreen.Models.Models;
+using webgreen.Repository.IRepository;
+
+namespace webgreen.Repository
+{
+    public class CatsRepository : Repository<cats>, ICatsRepository
+    {
+        private ApplicationDbContext data;
+        public CatsRepository(ApplicationDbContext db) : base(db)
+        {
+            this.data = db;
+        }
+
+        public void save()
+        {
+           data.SaveChanges();
+        }
+
+        public void update(cats dat)
+        {
+            data.categories.Update(dat);
+        }
+    }
+}
---
+++ webgreen/Repository/IRepository/ICatsRepository.cs
@@ -0,0 +1,15 @@
+﻿using webgreen.Models.Models;
+
+namespace webgreen.Repository.IRepository
+{
+    public interface ICatsRepository:IRepository<cats>
+
+    {
+        void update(cats data);
+        void save();
+
+        
+        
+    }
+
+}
